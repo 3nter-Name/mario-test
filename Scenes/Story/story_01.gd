790466b5af5fbb21aa8952013b6f47968d35608f
Enter-Name <Enter-Name@outlook.com>
2026-05-12 14:59:54 -0400

    Fresh Start
    Starting over, everything is still saved in main for now and I also have a local backup

--- Scenes/Story/story_01.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
-
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu.tscn");
